6caf3b492d885ff8bdbc068021742a09198b238b
Mehul Basu <mbasu5@wisc.edu>
2025-03-19 18:22:06 -0500

    signout btn in dashboard

--- networks-frontend/src/components/Dashboard.jsx
+++ networks-frontend/src/components/Dashboard.jsx
@@ -1,11 +1,22 @@
-import { Container, Title, Divider, Stack } from '@mantine/core';
+import { Container, Title, Divider, Stack, Button } from '@mantine/core';
+import { auth } from '../utils/firebase';
 import UploadButton from './UploadButton';
 import './Dashboard.scss';
 
 function Dashboard() {
+
+    const signOut = () => {
+        auth.signOut().then(() => {
+            console.log("User signed out");
+        }).catch((error) => {
+            console.error(error.code, error.message);
+        });
+    }
+
     return (
         <>
             <Container className='page-container'>
+                <Button onClick={signOut}>Log Out</Button>
                 <Stack spacing='xl'>
                     <Title ta='center'>Flix</Title>
                     <UploadButton />
