d03c9c938f699fdd309804fec89547731ab7b884
Mehul Basu <mbasu5@wisc.edu>
2025-04-20 22:38:43 -0500

    download working for gallery

--- networks-client-backend/app.js
+++ networks-client-backend/app.js
@@ -19,9 +19,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// In your app.js
 const uploadRouter = require('./uploads');
 app.use('/upload', uploadRouter);
+const downloadRouter = require('./downloads');
+app.use('/download', downloadRouter);
 
 /* GET home page. */
 app.get('/', function(req, res, next) {
